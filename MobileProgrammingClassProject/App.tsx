71730dc7212f88eb8132542ec6c17a95a5cde840
Karoll Alberto <misaperez05@unitec.edu>
2026-06-24 18:31:37 -0600

    Correccion de libbrerias mal importadas o innecesarias

--- MobileProgrammingClassProject/App.tsx
+++ MobileProgrammingClassProject/App.tsx
@@ -1,14 +1,19 @@
 import { NavigationContainer } from "@react-navigation/native";
-import { CaremapHealthProvider } from "./src/contexts/CaremapHealthContexts";
 import { Provider } from "react-redux";
 import { store } from "./src/store";
+
+import { CaremapHealthProvider } from "./src/contexts/CaremapHealthContexts";
 import StackNavigator from "./src/navigation/StackNavigator";
 import { navigationRef } from "./src/navigation/NavigationService";
 
 export default function App() {
   return (
-    <NavigationContainer>
-      <StackNavigator />
-    </NavigationContainer>
+    <Provider store={store}>
+      <CaremapHealthProvider>
+        <NavigationContainer ref={navigationRef}>
+          <StackNavigator />
+        </NavigationContainer>
+      </CaremapHealthProvider>
+    </Provider>
   );
 }
